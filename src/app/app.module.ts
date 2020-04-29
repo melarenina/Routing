5a680aca777dafa4f213fd3cac29986efbcd60ff
MML8CA <mml8ca@bosch.com>
2020-04-29 15:47:29 -0300

    Outsourcing the Route Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,26 +12,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
 import { ServersService } from './Services/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-
-export const appRoutes: Routes = [
-  // The path is what will come after the domain
-  // In this case, it would be localhost:4200/users, localhost:4200 e localhost:4200/servers
-
-  // Path  -  Action that will happen
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },
-  ] },
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id/edit', component: EditServerComponent },
-      { path: ':id', component: ServerComponent },
-    ]
-  },
-  { path: 'not-found', component: PageNotFoundComponent},
-  // This rout must always be the last one
-  { path: '**', redirectTo: '/not-found', pathMatch: 'full'},
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -47,8 +28,7 @@ export const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    // ForRoot allows us to register some routes to our main application
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
