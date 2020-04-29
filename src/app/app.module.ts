57d398cd3c0ba1919b8025043a098f6c7e61a22e
MML8CA <mml8ca@bosch.com>
2020-04-29 11:23:31 -0300

    Setting up and Loading Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,17 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
 import { ServersService } from './Services/servers.service';
+
+
+export const appRoutes: Routes = [
+  // The path is what will come after the domain
+  // In this case, it would be localhost:4200/users, localhost:4200 e localhost:4200/servers
+
+  // Path  -  Action that will happen
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+];
 
 @NgModule({
   declarations: [
@@ -24,6 +36,8 @@ import { ServersService } from './Services/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    // ForRoot allows us to register some routes to our main application
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
