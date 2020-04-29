1f2c16dfe37b8e60474f2f8d056610beaecba808
MML8CA <mml8ca@bosch.com>
2020-04-29 15:33:22 -0300

    Redirecting and Wildcard Routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   }
 
   onLoadServer(id: number){
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: id === 3 ? '1' : '0'}, fragment: 'loading'});
   }
 
 }
